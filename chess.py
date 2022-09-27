17d06320bff0d89ee258397e249a6eabe3fcc8d5
mohammed <zeggafmohah@gmail.com>
2022-09-27 15:53:20 +0100

    too long

--- chess.py
+++ chess.py
@@ -1,6 +1,7 @@
 from shutil import move
 import pygame
 from sys import exit
+import itertools
 
 pygame.init()
 
@@ -21,60 +22,52 @@ rookB = pygame.image.load('graphics/rookB.png').convert_alpha()
 pawnW = pygame.image.load('graphics/pawnW.png').convert_alpha()
 pawnB = pygame.image.load('graphics/pawnB.png').convert_alpha()
 
-pieces = [kingW, kingB, queenW, queenB, bishopW, bishopB, knightW, knightB, rookW, rookB, pawnW, pawnB]
+# rookW_rect = rookW.get_rect(topleft=(7, 7))
+# knightW_rect = knightW.get_rect(topleft=(82, 7))
+# bishopW_rect = bishopW.get_rect(topleft=(157, 7))
+# kingW_rect = kingW.get_rect(topleft=(232, 7))
+# queenW_rect = queenW.get_rect(topleft=(307, 7))
+# bishopW_rect = bishopW.get_rect(topleft=(382, 7))
+# knightW_rect = knightW.get_rect(topleft=(457, 7))
+# rookW_rect = rookW.get_rect(topleft=(532, 7))
 
-# class Piece:
-#     def move(self):
-#         for p in pieces:
-            
-screen.blit(chess_surf, (0, 0))
-screen.blit(rookW, (7, 7))
-screen.blit(knightW, (82, 7)) 
-screen.blit(bishopW, (157, 7))
-screen.blit(kingW, (232, 7))  
-screen.blit(queenW, (307, 7))
-screen.blit(bishopW, (382, 7))
-screen.blit(knightW, (457, 7))
-screen.blit(rookW, (532, 7))
-screen.blit(pawnW, (7, 82))
-screen.blit(pawnW, (82, 82)) 
-screen.blit(pawnW, (157, 82))
-screen.blit(pawnW, (232, 82))
-screen.blit(pawnW, (307, 82))
-screen.blit(pawnW, (382, 82))
-screen.blit(pawnW, (457, 82))
-screen.blit(pawnW, (532, 82))
+# rookB_rect = rookB.get_rect(topleft=(7, 533))
+# knightB_rect = knightB.get_rect(topleft=(82, 533)) 
+# bishopB_rect = bishopB.get_rect(topleft=(157, 533))
+# kingB_rect = kingB.get_rect(topleft=(232, 533))    
+# queenB_rect = queenB.get_rect(topleft=(307, 533))
+# bishopB_rect = bishopB.get_rect(topleft=(382, 533))
+# knightB_rect = knightB.get_rect(topleft=(457, 533))
+# rookB_rect = rookB.get_rect(topleft=(532, 533))
 
-screen.blit(rookB, (7, 533))
-screen.blit(knightB, (82, 533)) 
-screen.blit(bishopB, (157, 533))
-screen.blit(kingB, (232, 533))  
-screen.blit(queenB, (307, 533))
-screen.blit(bishopB, (382, 533))
-screen.blit(knightB, (457, 533))
-screen.blit(rookB, (532, 533))
-screen.blit(pawnB, (7, 458))
-screen.blit(pawnB, (82, 458)) 
-screen.blit(pawnB, (157, 458))
-screen.blit(pawnB, (232, 458))
-screen.blit(pawnB, (307, 458))
-screen.blit(pawnB, (382, 458))
-screen.blit(pawnB, (457, 458))
-screen.blit(pawnB, (532, 458))
+piecesA = [rookW, knightW, bishopW, kingW, queenW, bishopW, knightW, rookW]
+piecesB = [rookB, knightB, bishopB, kingB, queenB, bishopB, knightB, rookB]
+# pieces_rectA = [rookW_rect, knightW_rect, bishopW_rect, kingW_rect, queenW_rect, bishopW_rect, knightW_rect, rookW_rect]
+# pieces_rectB = [rookB_rect, knightB_rect, bishopB_rect, kingB_rect, queenB_rect, bishopB_rect, knightB_rect, rookB_rect]
+
+# for x in piecesA:
+#     x _rect = x.get_rect(topleft=(str(y), 533))
+#     y += 75
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+ 
+    # for p in pieces: 
+    #     if event.type == pygame.MOUSEBUTTONDOWN and p.collidepoint(pygame.mouse.get_pos()):
+    #         p.pos = pygame.mouse.get_pos()
 
+    x1 = 7
+    for pA in piecesA[0:8:]:
+        screen.blit(pA, pA.get_rect(topleft=(x1, 533)))
+        x1 += 75
 
-
-    
-    for p in pieces: 
-        if event.type == pygame.MOUSEBUTTONDOWN and p.collidepoint(pygame.mouse.get_pos()):
-            p.pos = pygame.mouse.get_pos()
-
+    x1 = 7
+    for pB in piecesB:
+        screen.blit(pB, pB.get_rect(topleft=(x1, 7)))
+        x1 += 75
 
     pygame.display.update()
     clock.tick(60)
